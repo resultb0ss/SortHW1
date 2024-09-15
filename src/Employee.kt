e1dc76e5ce408fdf6d4ec2b132e38339c926af81
Admin <reteamboss@gmail.com>
2024-09-15 12:57:04 +0300

    1

--- src/Employee.kt
+++ src/Employee.kt
@@ -1,2 +1,7 @@
-class Employee {
+class Employee(var name: String, var age: Int, var salary: Int) {
+
+
+    override fun toString(): String {
+        return "Имя: $name, Возраст: $age, Зарплата: $salary"
+    }
 }
--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,20 @@
 fun main() {
-    println("Hello World!")
+    var empOne = Employee("Виктор", 22,10000)
+    var empTwo = Employee("Дмитрий", 32,34433)
+    var empThree = Employee("Екатерина", 20,33554)
+    var empFour = Employee("Елена", 23,64343)
+    var empFive = Employee("Андрей", 25,135466)
+
+    var array = arrayOf(empOne,empTwo,empThree,empFour,empFive)
+
+
+    val sortedEmployeeName = array.sortedWith(compareBy({it.name}))
+    val sortedEmployeeAge = array.sortedWith(compareBy { it.age })
+    val sortedEmployeeSalary = array.sortedWith(compareBy { it.salary } )
+
+    println(sortedEmployeeSalary.toString()) // Сортировка по зп
+    println("-----------------------")
+    println(sortedEmployeeAge.toString()) // Сортировка по Возрасту
+    println("---------------------")
+    println(sortedEmployeeName.toString()) // Сортировка по имени
 }
